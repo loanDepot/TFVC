8342c88f41aa15e78030106a8c60f23c2392f7f7
Kevin Marquette <kmarquette@loandepot.com>
2018-10-17 16:09:21 -0700

    more error handling

--- build.ps1
+++ build.ps1
@@ -36,13 +36,17 @@ Get-ChildItem Env:APPVEYOR*
 $Error.Clear()
 
 'Invoking build'
-
-Invoke-Build $Task -Result 'Result'
-if ($Result.Error)
+try
 {
-    $Error[-1] | Format-List -Force
-    $Error[0] | Format-List -Force
-
+    Invoke-Build $Task -Result 'Result'
+}
+catch
+{
+    if ($Result.Error)
+    {
+        $Error[-1] | Format-List -Force
+        $Error[0] | Format-List -Force
+    }
     "Attempting module import to get better error message for common issues"
     $path = Resolve-Path './Output/*/*.psd1'
     Import-Module $path -Verbose -Force
